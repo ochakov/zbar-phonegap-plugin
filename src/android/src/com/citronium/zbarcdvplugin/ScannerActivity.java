2e4661d9d6d8ddfc4419d4eb9ad26108f5d46142
hukal <konstantin.khukalenko@gmail.com>
2014-04-02 17:59:45 +0400

    Fixed import

--- src/android/src/com/citronium/zbarcdvplugin/ScannerActivity.java
+++ src/android/src/com/citronium/zbarcdvplugin/ScannerActivity.java
@@ -1,16 +1,8 @@
 package com.citronium.zbarcdvplugin;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.content.Intent;
 
 import com.citronium.zbarcdvplugin.zbar.Result;
 import com.citronium.zbarcdvplugin.zbar.ZBarScannerView;
